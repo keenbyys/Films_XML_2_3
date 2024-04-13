6d1d6a3dba50fb9d67e0e7682c3bf5a536e50ae9
keenbyys <166899952+keenbyys@users.noreply.github.com>
2024-04-13 10:16:40 +0300

    Load files

---
+++ CinemaCourse1/Form1.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace CinemaCourse1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxNameFilm = new System.Windows.Forms.TextBox();
+            this.numericUpDownDuration = new System.Windows.Forms.NumericUpDown();
+            this.comboBoxGenre = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxBeginning = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBoxCinema = new System.Windows.Forms.ComboBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonLoadXml = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonSaveAsXml = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Film = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cinema = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Genre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Beginning = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Duration = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBoxDate = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDuration)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxNameFilm
+            // 
+            this.textBoxNameFilm.Location = new System.Drawing.Point(67, 9);
+            this.textBoxNameFilm.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxNameFilm.Multiline = true;
+            this.textBoxNameFilm.Name = "textBoxNameFilm";
+            this.textBoxNameFilm.Size = new System.Drawing.Size(277, 24);
+            this.textBoxNameFilm.TabIndex = 0;
+            // 
+            // numericUpDownDuration
+            // 
+            this.numericUpDownDuration.Location = new System.Drawing.Point(616, 52);
+            this.numericUpDownDuration.Margin = new System.Windows.Forms.Padding(4);
+            this.numericUpDownDuration.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numericUpDownDuration.Name = "numericUpDownDuration";
+            this.numericUpDownDuration.Size = new System.Drawing.Size(112, 24);
+            this.numericUpDownDuration.TabIndex = 1;
+            // 
+            // comboBoxGenre
+            // 
+            this.comboBoxGenre.FormattingEnabled = true;
+            this.comboBoxGenre.Items.AddRange(new object[] {
+            "Horror",
+            "Action",
+            "Drama"});
+            this.comboBoxGenre.Location = new System.Drawing.Point(433, 7);
+            this.comboBoxGenre.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBoxGenre.Name = "comboBoxGenre";
+            this.comboBoxGenre.Size = new System.Drawing.Size(110, 26);
+            this.comboBoxGenre.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Film:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(359, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Genre:";
+            // 
+            // textBoxBeginning
+            // 
+            this.textBoxBeginning.Location = new System.Drawing.Point(352, 48);
+            this.textBoxBeginning.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxBeginning.Multiline = true;
+            this.textBoxBeginning.Name = "textBoxBeginning";
+            this.textBoxBeginning.Size = new System.Drawing.Size(163, 26);
+            this.textBoxBeginning.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(243, 48);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 24);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Beginning:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(523, 50);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 24);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Duration:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(37, 50);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 24);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Cinema:";
+            // 
+            // comboBoxCinema
+            // 
+            this.comboBoxCinema.FormattingEnabled = true;
+            this.comboBoxCinema.Items.AddRange(new object[] {
+            "EvilLight",
+            "VibeJO",
+            "RestlessDreams"});
+            this.comboBoxCinema.Location = new System.Drawing.Point(125, 48);
+            this.comboBoxCinema.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBoxCinema.Name = "comboBoxCinema";
+            this.comboBoxCinema.Size = new System.Drawing.Size(110, 26);
+            this.comboBoxCinema.TabIndex = 10;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(207, 98);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(114, 25);
+            this.buttonAdd.TabIndex = 12;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(327, 98);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(114, 25);
+            this.buttonEdit.TabIndex = 13;
+            this.buttonEdit.Text = "Edit";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(447, 98);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(114, 25);
+            this.buttonDelete.TabIndex = 14;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonLoadXml
+            // 
+            this.buttonLoadXml.Location = new System.Drawing.Point(447, 357);
+            this.buttonLoadXml.Name = "buttonLoadXml";
+            this.buttonLoadXml.Size = new System.Drawing.Size(114, 25);
+            this.buttonLoadXml.TabIndex = 17;
+            this.buttonLoadXml.Text = "Load XML";
+            this.buttonLoadXml.UseVisualStyleBackColor = true;
+            this.buttonLoadXml.Click += new System.EventHandler(this.buttonLoadXml_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(327, 357);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(114, 25);
+            this.buttonClear.TabIndex = 16;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonSaveAsXml
+            // 
+            this.buttonSaveAsXml.Location = new System.Drawing.Point(207, 357);
+            this.buttonSaveAsXml.Name = "buttonSaveAsXml";
+            this.buttonSaveAsXml.Size = new System.Drawing.Size(114, 25);
+            this.buttonSaveAsXml.TabIndex = 15;
+            this.buttonSaveAsXml.Text = "Save as XML";
+            this.buttonSaveAsXml.UseVisualStyleBackColor = true;
+            this.buttonSaveAsXml.Click += new System.EventHandler(this.buttonSaveAsXml_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Film,
+            this.Cinema,
+            this.Genre,
+            this.Date,
+            this.Beginning,
+            this.Duration});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 129);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(752, 222);
+            this.dataGridView1.TabIndex = 18;
+            this.dataGridView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseClick);
+            // 
+            // Film
+            // 
+            this.Film.HeaderText = "Film";
+            this.Film.Name = "Film";
+            this.Film.Width = 125;
+            // 
+            // Cinema
+            // 
+            this.Cinema.HeaderText = "Cinema";
+            this.Cinema.Name = "Cinema";
+            this.Cinema.Width = 125;
+            // 
+            // Genre
+            // 
+            this.Genre.HeaderText = "Genre";
+            this.Genre.Name = "Genre";
+            this.Genre.Width = 125;
+            // 
+            // Date
+            // 
+            this.Date.HeaderText = "Date";
+            this.Date.Name = "Date";
+            this.Date.Width = 125;
+            // 
+            // Beginning
+            // 
+            this.Beginning.HeaderText = "Beginning";
+            this.Beginning.Name = "Beginning";
+            this.Beginning.Width = 125;
+            // 
+            // Duration
+            // 
+            this.Duration.HeaderText = "Duration";
+            this.Duration.Name = "Duration";
+            this.Duration.Width = 125;
+            // 
+            // textBoxDate
+            // 
+            this.textBoxDate.Location = new System.Drawing.Point(616, 7);
+            this.textBoxDate.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxDate.Multiline = true;
+            this.textBoxDate.Name = "textBoxDate";
+            this.textBoxDate.Size = new System.Drawing.Size(128, 26);
+            this.textBoxDate.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(557, 10);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 24);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Date:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(776, 406);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxDate);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonLoadXml);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonSaveAsXml);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBoxCinema);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxBeginning);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxGenre);
+            this.Controls.Add(this.numericUpDownDuration);
+            this.Controls.Add(this.textBoxNameFilm);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDuration)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxNameFilm;
+        private System.Windows.Forms.NumericUpDown numericUpDownDuration;
+        private System.Windows.Forms.ComboBox comboBoxGenre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxBeginning;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBoxCinema;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonLoadXml;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonSaveAsXml;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBoxDate;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Film;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cinema;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Genre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Date;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Beginning;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Duration;
+    }
+}
+
---
+++ CinemaCourse1/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CinemaCourse1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (textBoxNameFilm.Text == "")
+            {
+                MessageBox.Show("Error!");
+            } 
+            else
+            {
+                int n  = dataGridView1.Rows.Add();
+                dataGridView1.Rows[n].Cells[0].Value = textBoxNameFilm.Text;
+                dataGridView1.Rows[n].Cells[1].Value = comboBoxCinema.Text;
+                dataGridView1.Rows[n].Cells[2].Value = comboBoxGenre.Text;
+                dataGridView1.Rows[n].Cells[3].Value = textBoxDate.Text;
+                dataGridView1.Rows[n].Cells[4].Value = textBoxBeginning.Text;
+                dataGridView1.Rows[n].Cells[5].Value = numericUpDownDuration.Text;
+            }
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int n = dataGridView1.SelectedRows[0].Index;
+                dataGridView1.Rows[n].Cells[0].Value = textBoxNameFilm.Text;
+                dataGridView1.Rows[n].Cells[1].Value = comboBoxCinema.Text;
+                dataGridView1.Rows[n].Cells[2].Value = comboBoxGenre.Text;
+                dataGridView1.Rows[n].Cells[3].Value = textBoxDate.Text;
+                dataGridView1.Rows[n].Cells[4].Value = textBoxBeginning.Text;
+                dataGridView1.Rows[n].Cells[5].Value = numericUpDownDuration.Text;
+            } 
+            else
+            {
+                MessageBox.Show("Error!");
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                dataGridView1.Rows.RemoveAt(dataGridView1.SelectedRows[0].Index);
+            }
+            else
+            {
+                MessageBox.Show("Error!");
+            }
+        }
+
+        private void dataGridView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBoxNameFilm.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            comboBoxCinema.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            comboBoxGenre.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+            textBoxDate.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+            textBoxBeginning.Text = dataGridView1.SelectedRows[0].Cells[4].Value.ToString();
+            int n = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells[5].Value);
+            numericUpDownDuration.Value = n;
+        }
+
+        private void buttonSaveAsXml_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataSet dataSet = new DataSet();
+                DataTable dataTable = new DataTable();
+                dataTable.TableName = "Movie Show";
+                dataTable.Columns.Add("Film");
+                dataTable.Columns.Add("Cinema");
+                dataTable.Columns.Add("Genre");
+                dataTable.Columns.Add("Date");
+                dataTable.Columns.Add("Beginnig");
+                dataTable.Columns.Add("Duration");
+                
+                dataSet.Tables.Add(dataTable);
+
+                foreach (DataGridViewRow r in dataGridView1.Rows)
+                {
+                    DataRow row = dataSet.Tables["Movie Show"].NewRow();
+                    row["Film"] = r.Cells[0].Value;
+                    row["Cinema"] = r.Cells[1].Value;
+                    row["Genre"] = r.Cells[2].Value;
+                    row["Date"] = r.Cells[3].Value;
+                    row["Beginnig"] = r.Cells[4].Value;
+                    row["Duration"] = r.Cells[5].Value;
+
+                    dataSet.Tables["Movie Show"].Rows.Add(row);
+                }
+                dataSet.WriteXml("D:\\Data.xml");
+                MessageBox.Show("XML-file is saved!", "Done!");
+            } 
+            catch 
+            {
+                MessageBox.Show("Could not save XML-file", "Error!");
+            }
+        }
+
+        private void buttonLoadXml_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                MessageBox.Show("Please, clear the field before loading new XML-file!", "Error!");
+            }
+            else
+            {
+                if(File.Exists("D:\\Data.xml"))
+                {
+                    DataSet dataSet = new DataSet();
+                    dataSet.ReadXml("D:\\Data.xml");
+
+                    foreach (DataRow item in dataSet.Tables["Movie Show"].Rows)
+                    {
+                        int n = dataGridView1.Rows.Add();
+                        dataGridView1.Rows[n].Cells[0].Value = item["Film"];
+                        dataGridView1.Rows[n].Cells[1].Value = item["Cinema"];
+                        dataGridView1.Rows[n].Cells[2].Value = item["Genre"];
+                        dataGridView1.Rows[n].Cells[3].Value = item["Date"];
+                        dataGridView1.Rows[n].Cells[4].Value = item["Beginnig"];
+                        dataGridView1.Rows[n].Cells[5].Value = item["Duration"];
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("XML-file not found!", "Error!");
+                }
+            }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                dataGridView1.Rows.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Table is empty!", "Error!");
+            }
+        }
+    }
+}
